c7ba8134265896eeb4274f8f621e4c3de3639265
Christianib003 <christian.byiringiro.sp@dlh.de>
2025-01-24 20:19:20 +0200

    solve challenges

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+""""
+This is module for 
+adding matrices
+"""
+
+
+def check_shape(arr):
+    shape_arr = []
+
+    while(isinstance(arr,list)):
+        shape_arr.append(len(arr))
+        arr = arr[0]
+    
+    return shape_arr
+
+def add_matrices2D(mat1, mat2):
+
+    """"
+    This is for addition
+    """
+
+
+    sum = []
+
+    if(check_shape(mat1) != check_shape(mat2)):
+        return None
+    
+    for i in range(len(mat1)):
+        sum_row = []
+        for j in range(len(mat1)):
+            sum_row.append(mat1[i][j] + mat2[i][j])
+        
+        sum.append(sum_row)
+
+    return sum
